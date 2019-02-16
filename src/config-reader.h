a6a7ea8fce0eca1588f6996a9b1fb336b326f7b9
YihanWangAstro <yihan.wang.1@stonybrook.edu>
2019-02-16 16:48:58 -0500

    command line reader

--- src/config-reader.h
+++ src/config-reader.h
@@ -8,12 +8,13 @@
 #include <fstream>
 #include <unordered_map>
 #include <algorithm>
+#include <sstream>
 #include "dev_tools.h"
 
 namespace SpaceH {
-    class ConfigReader : public std::unordered_map<std::string, double> {
+    /*class ConfigReader : public std::unordered_map<std::string, double> {
     public:
-        ConfigReader(std::string const &file_name, char divider = '=', char commenter = '#') {
+        explicit ConfigReader(std::string const &file_name, char divider = '=', char commenter = '#') {
             std::fstream file(file_name);
             if (file.is_open()) {
                 std::string line;
@@ -41,7 +42,78 @@ namespace SpaceH {
                 SpaceH::print(os, key, '=', value, '\n');
             }
         }
+    };*/
+
+    class ConfigReader {
+    public:
+        explicit ConfigReader(std::string const &file_name, char divider = '=', char commenter = '#') {
+            std::fstream file(file_name);
+            if (file.is_open()) {
+                std::string line;
+                while (std::getline(file, line)) {
+                    line.erase(std::remove_if(line.begin(), line.end(), isspace), line.end());
+
+                    if (line[0] == commenter || line.empty())
+                        continue;
+
+                    auto devider_pos = line.find(divider);
+                    auto key = line.substr(0, devider_pos);
+                    auto value = line.substr(devider_pos + 1);
+                    map_[key] = value;
+                }
+            } else {
+                SPACEHUB_ABORT("Cannot open the configure file: ", file_name, "\r\n");
+            }
+        }
+
+        explicit ConfigReader(char const *file_name, char divider = '=', char commenter = '#')
+                : ConfigReader(std::string(file_name), divider, commenter) {};
+
+        template <typename T>
+        T get(std::string const& key) {
+            auto iskey = (map_.end() != map_.find(key));
+            if (iskey) {
+                std::stringstream ss(map_[key]);
+                T value;
+                ss >> value;
+                return value;
+            } else {
+                SPACEHUB_ABORT("Invalid key for configure file!");
+            }
+
+        }
+        /*
+        auto operator[](std::string const& key) {
+            std::stringstream ss(map_[key]);
+            decltype( operator[](key)) value;
+            ss >> value;
+            return value;
+        }*/
+
+        friend std::ostream &operator<<(std::ostream &os, ConfigReader const &config) {
+            for (auto&[key, value] : config.map_) {
+                SpaceH::print(os, key, '=', value, '\n');
+            }
+        }
+
+    private:
+        std::unordered_map<std::string, std::string> map_;
     };
+
+    template<typename ...Args>
+    void read_command_line(int argc, char **argv, Args &&...args) {
+        constexpr size_t n = sizeof...(Args);
+
+        if (argc != n + 1) {
+            SPACEHUB_ABORT("Wrong args number!");
+        } else {
+            std::stringstream ss;
+            for (int i = 1; i < argc; ++i) {
+                ss << argv[i] << ' ';
+            }
+            ((ss >> std::forward<Args>(args)),...);
+        }
+    }
 }
 
 
--- src/multi-thread/multi-thread.h
+++ src/multi-thread/multi-thread.h
@@ -12,85 +12,83 @@
 #include <iostream>
 #include <tuple>
 #include "../dev_tools.h"
-namespace SpaceH {
-    namespace MultiThread {
 
-        const size_t auto_thread = (std::thread::hardware_concurrency() > 1) ? std::thread::hardware_concurrency() : 1;
+namespace SpaceH::MultiThread {
 
-        template<typename Lambda>
-        void multi_threads_loop(size_t total_len,  size_t thread_num, Lambda &&task) {
-            auto len_pth = total_len / thread_num;
-            std::vector<std::thread> threads;
-            threads.reserve(thread_num);
-            for (size_t th_id = 0; th_id < thread_num; ++th_id) {
-                size_t begin = th_id * len_pth;
-                size_t end   = begin + len_pth;
-                if(end + len_pth > total_len)
-                    end = total_len;
-                threads.emplace_back(std::thread(std::forward<Lambda>(task), begin, end));
-            }
+    const size_t auto_thread = (std::thread::hardware_concurrency() > 1) ? std::thread::hardware_concurrency() : 1;
 
-            for (auto &thread : threads) {
-                if (thread.joinable())
-                    thread.join();
+    template<typename Lambda>
+    void multi_threads_loop(size_t total_len, size_t thread_num, Lambda &&task) {
+        auto len_pth = total_len / thread_num;
+        std::vector<std::thread> threads;
+        threads.reserve(thread_num);
+        for (size_t th_id = 0; th_id < thread_num; ++th_id) {
+            size_t begin = th_id * len_pth;
+            size_t end = begin + len_pth;
+            if (end + len_pth > total_len)
+                end = total_len;
+            threads.emplace_back(std::thread(std::forward<Lambda>(task), begin, end));
+        }
+
+        for (auto &thread : threads) {
+            if (thread.joinable())
+                thread.join();
+        }
+    }
+
+    template<typename Callable, typename ...Args>
+    void multi_thread_run(size_t thread_num, Callable &&job, Args &&...args) {
+        std::vector<std::thread> threads;
+        threads.reserve(thread_num);
+        for (size_t i = 0; i < thread_num; ++i) {
+            threads.emplace_back(std::thread(std::forward<Callable>(job), std::forward<Args>(args)...));
+        }
+
+        for (auto &th : threads) {
+            if (th.joinable())
+                th.join();
+        }
+    }
+
+    template<typename ...Args>
+    void auto_multi_thread(Args &&... args) {
+        multi_thread_run(auto_thread, std::forward<Args>(args)...);
+    }
+
+    class ConcurrentFile {
+    public:
+        ConcurrentFile(const char *file_name, std::ios_base::openmode mode) :
+                file_(std::make_shared<std::fstream>(file_name, mode)), mutex_(std::make_shared<std::mutex>()) {
+            if (!file_->is_open()) {
+                SPACEHUB_ABORT("Unable to open file: ", file_name);
             }
         }
 
-        template<typename Callable, typename ...Args>
-        void multi_thread_run(size_t thread_num, Callable &&job, Args &&...args) {
-            std::vector<std::thread> threads;
-            threads.reserve(thread_num);
-            for (size_t i = 0; i < thread_num; ++i) {
-                threads.emplace_back(std::thread(std::forward<Callable>(job), std::forward<Args>(args)...));
-            }
-
-            for (auto &th : threads) {
-                if (th.joinable())
-                    th.join();
-            }
-        }
-
-        template <typename ...Args>
-        void auto_multi_thread(Args&&... args){
-            multi_thread_run(auto_thread, std::forward<Args>(args)...);
-        }
-
-        class ConcurrentFile {
-        public:
-            ConcurrentFile(const char *file_name, std::ios_base::openmode mode) :
-                    file_(std::make_shared<std::fstream>(file_name, mode)), mutex_(std::make_shared<std::mutex>()) {
-                if (!file_->is_open()) {
-                    std::cout << "Unable to open file: " << std::string(file_name) << "\n";
-                    exit(0);
-                }
-            }
-
-            ConcurrentFile(const std::string &file_name, std::ios_base::openmode mode) :
+        ConcurrentFile(const std::string &file_name, std::ios_base::openmode mode) :
                 ConcurrentFile(file_name.c_str(), mode) {}
 
-            template<typename Callback, typename ...Args>
-            auto execute(Callback &&func, Args &&...args) {
-                std::lock_guard<std::mutex> lock(*mutex_);
-                return func(*file_, std::forward<Args>(args)...);
-            }
+        template<typename Callback, typename ...Args>
+        auto execute(Callback &&func, Args &&...args) {
+            std::lock_guard<std::mutex> lock(*mutex_);
+            return func(*file_, std::forward<Args>(args)...);
+        }
 
-            template<typename U>
-            friend ConcurrentFile &operator<<(ConcurrentFile &os, U &&tup) {
-                std::lock_guard<std::mutex> lock(*(os.mutex_));
-                *(os.file_) << std::forward<decltype(tup)>(tup);
-            }
+        template<typename U>
+        friend ConcurrentFile &operator<<(ConcurrentFile &os, U &&tup) {
+            std::lock_guard<std::mutex> lock(*(os.mutex_));
+            *(os.file_) << std::forward<decltype(tup)>(tup);
+        }
 
-            template<typename U>
-            friend ConcurrentFile &operator>>(ConcurrentFile &is, U &&tup) {
-                std::lock_guard<std::mutex> lock(*(is.mutex_));
-                *(is.file_) >> std::forward<decltype(tup)>(tup);
-            }
+        template<typename U>
+        friend ConcurrentFile &operator>>(ConcurrentFile &is, U &&tup) {
+            std::lock_guard<std::mutex> lock(*(is.mutex_));
+            *(is.file_) >> std::forward<decltype(tup)>(tup);
+        }
 
-        private:
-            std::shared_ptr<std::fstream> file_;
-            std::shared_ptr<std::mutex> mutex_;
-        };
-    }
+    private:
+        std::shared_ptr<std::fstream> file_;
+        std::shared_ptr<std::mutex> mutex_;
+    };
 }
 
 #endif
