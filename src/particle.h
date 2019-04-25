bf853f148ac5c24c4304e57d5a6a854ee0c72d8e
YihanWangAstro <yihan.wang.1@stonybrook.edu>
2019-04-25 18:53:15 -0400

    format

--- src/particle.h
+++ src/particle.h
@@ -8,68 +8,121 @@
 
 namespace space {
 
+    /*---------------------------------------------------------------------------*\
+        Class PointParticle Declaration
+    \*---------------------------------------------------------------------------*/
     template<typename Real>
     struct PointParticle {
     public:
+        //type members
         using Scalar = Real;
         using Vector = Vec3<Scalar>;
 
+        //constructors
         PointParticle() = default;
 
-        explicit PointParticle(Scalar m, Vector p, Vector v)
-                : pos(p), vel(v), mass(m) {}
+        explicit PointParticle(Scalar m, Vector p, Vector v);
 
-        explicit PointParticle(Scalar m, Scalar px = 0, Scalar py = 0, Scalar pz = 0, Scalar vx = 0, Scalar vy = 0, Scalar vz = 0)
-                : pos(px, py, pz), vel(vx, vy, vz), mass(m) {}
+        explicit PointParticle(Scalar m, Scalar px = 0, Scalar py = 0, Scalar pz = 0, Scalar vx = 0, Scalar vy = 0,
+                               Scalar vz = 0);
 
-        friend std::ostream &operator<<(std::ostream &os, PointParticle const &particle) {
-            space::display(os, particle.mass, particle.pos, particle.vel);
-            return os;
-        }
+        //friend methods
+        friend std::ostream &operator<<(std::ostream &os, PointParticle const &particle);
 
-        friend std::istream &operator>>(std::istream &is, PointParticle &particle) {
-            space::input(is, particle.mass, particle.pos, particle.vel);
-            return is;
-        }
+        friend std::istream &operator>>(std::istream &is, PointParticle &particle);
 
+        //public members
         Vector pos;
         Vector vel;
         Scalar mass;
     };
 
-    template <typename Derived>
+    /*---------------------------------------------------------------------------*\
+        Class PointParticle Implementation
+    \*---------------------------------------------------------------------------*/
+    template<typename Real>
+    PointParticle<Real>::PointParticle(Scalar m, PointParticle::Vector p, PointParticle::Vector v)
+            : pos(p), vel(v), mass(m) {}
+
+    template<typename Real>
+    PointParticle<Real>::PointParticle(Scalar m, Scalar px, Scalar py, Scalar pz, Scalar vx, Scalar vy, Scalar vz)
+            : pos(px, py, pz), vel(vx, vy, vz), mass(m) {}
+
+    template<typename Real>
+    std::ostream &operator<<(std::ostream &os, PointParticle<Real> const &particle) {
+        space::display(os, particle.mass, particle.pos, particle.vel);
+        return os;
+    }
+
+    template<typename Real>
+    std::istream &operator>>(std::istream &is, PointParticle<Real> &particle) {
+        space::input(is, particle.mass, particle.pos, particle.vel);
+        return is;
+    }
+
+    /*---------------------------------------------------------------------------*\
+        Class SoAParticles Declaration
+    \*---------------------------------------------------------------------------*/
+    template<typename Derived>
     class SoAParticles {
     public:
+        //public methods
         DECLARE_CRTP_ACCESSOR(Derived, auto, mass);
+
         DECLARE_CRTP_ACCESSOR(Derived, auto, idn);
+
         DECLARE_CRTP_ACCESSOR(Derived, auto, time);
+
         DECLARE_CRTP_ACCESSOR(Derived, auto, pos);
+
         DECLARE_CRTP_ACCESSOR(Derived, auto, vel);
 
-        Derived& derived(){
-            return static_cast<Derived&>(*this);
-        }
+        Derived &derived();
 
-        void resize(size_t new_sz) {
-            static_cast<Derived*>(this)->impl_resize(new_sz);
-        }
+        void resize(size_t new_sz);
 
-        void reserve(size_t new_cap) {
-            static_cast<Derived*>(this)->impl_reserve(new_cap);
-        }
+        void reserve(size_t new_cap);
 
-        size_t number() const {
-            return static_cast<Derived const*>(this)->impl_number();
-        }
+        size_t number() const;
+
     private:
+        //constructors
         SoAParticles() = default;
+
         friend Derived;
     };
 
-    template <typename T>
+    /*---------------------------------------------------------------------------*\
+        Class SoAParticles Implementation
+    \*---------------------------------------------------------------------------*/
+    template<typename Derived>
+    Derived &SoAParticles<Derived>::derived() {
+        return static_cast<Derived &>(*this);
+    }
+
+    template<typename Derived>
+    void SoAParticles<Derived>::resize(size_t new_sz) {
+        static_cast<Derived *>(this)->impl_resize(new_sz);
+    }
+
+    template<typename Derived>
+    void SoAParticles<Derived>::reserve(size_t new_cap) {
+        static_cast<Derived *>(this)->impl_reserve(new_cap);
+    }
+
+    template<typename Derived>
+    size_t SoAParticles<Derived>::number() const {
+        return static_cast<Derived const *>(this)->impl_number();
+    }
+
+    /*---------------------------------------------------------------------------*\
+        Help functions and tools
+    \*---------------------------------------------------------------------------*/
+    template<typename T>
     constexpr bool is_soa_particles_v = std::is_base_of_v<SoAParticles<T>, T>;
 
 #define SPACEHUB_PARTICLE_TYPE_CHECK(CTR, VAL) static_assert(std::is_base_of_v<typename CTR::value_type, VAL>, "Class can only be initialized by containers with its internal 'Particle' type!");
+
 }
 #endif
 
