4371983083ee11c6692d9882b06a55a1a1cc0378
YihanWangAstro <yihan.wang.1@stonybrook.edu>
2019-10-01 13:49:45 -0400

    add hyperbolic orbit args

--- src/interaction.hpp
+++ src/interaction.hpp
@@ -5,107 +5,109 @@
 #ifndef SPACEHUB_EOM_H
 #define SPACEHUB_EOM_H
 
-#include "type-class.hpp"
 #include "core-computation.hpp"
+#include "type-class.hpp"
 
 namespace space {
 
-    /*---------------------------------------------------------------------------*\
-        Class Interactions Declaration
-    \*---------------------------------------------------------------------------*/
-    template<typename Derived>
-    class Interactions {
-    private:
-        //special macros to generate compile time method check
-        CREATE_METHOD_CHECK(impl_eval_extra_vel_indep_acc);
+/*---------------------------------------------------------------------------*\
+    Class Interactions Declaration
+\*---------------------------------------------------------------------------*/
+template <typename Derived>
+class Interactions {
+ private:
+  // special macros to generate compile time method check
+  CREATE_METHOD_CHECK(impl_eval_extra_vel_indep_acc);
 
-        CREATE_METHOD_CHECK(impl_eval_extra_vel_dep_acc);
-    public:
-        //public static members
-        /**
-         *
-         */
-        static constexpr bool ext_vel_dep{HAS_METHOD(Derived, impl_eval_extra_vel_dep_acc)};
+  CREATE_METHOD_CHECK(impl_eval_extra_vel_dep_acc);
 
-        /**
-         *
-         */
-        static constexpr bool ext_vel_indep{HAS_METHOD(Derived, impl_eval_extra_vel_indep_acc)};
+ public:
+  // public static members
+  /**
+   *
+   */
+  static constexpr bool ext_vel_dep{HAS_METHOD(Derived, impl_eval_extra_vel_dep_acc)};
 
-        //public method
-        /**
-         *
-         * @tparam Particles
-         * @param partc
-         * @param acc
-         */
-        template<typename Particles>
-        void eval_acc(Particles const &partc, typename Particles::Coord &acc);
+  /**
+   *
+   */
+  static constexpr bool ext_vel_indep{HAS_METHOD(Derived, impl_eval_extra_vel_indep_acc)};
 
-        /**
-         *
-         * @tparam Particles
-         * @param partc
-         * @param acc
-         */
-        template<typename Particles>
-        void eval_extra_vel_dep_acc(Particles const &partc, typename Particles::Coord &acc);
+  // public method
+  /**
+   *
+   * @tparam Particles
+   * @param partc
+   * @param acc
+   */
+  template <typename Particles>
+  void eval_acc(Particles const &partc, typename Particles::Coord &acc);
 
-        /**
-         *
-         * @tparam Particles
-         * @param partc
-         * @param acc
-         */
-        template<typename Particles>
-        void eval_extra_vel_indep_acc(Particles const &partc, typename Particles::Coord &acc);
+  /**
+   *
+   * @tparam Particles
+   * @param partc
+   * @param acc
+   */
+  template <typename Particles>
+  void eval_extra_vel_dep_acc(Particles const &partc, typename Particles::Coord &acc);
 
-        /**
-         *
-         * @tparam Particles
-         * @param partc
-         * @param acc
-         */
-        template<typename Particles>
-        void eval_newtonian_acc(Particles const &partc, typename Particles::Coord &acc);
-    private:
-        //constructors
-        Interactions() = default;
+  /**
+   *
+   * @tparam Particles
+   * @param partc
+   * @param acc
+   */
+  template <typename Particles>
+  void eval_extra_vel_indep_acc(Particles const &partc, typename Particles::Coord &acc);
 
-        friend Derived;
-    };
+  /**
+   *
+   * @tparam Particles
+   * @param partc
+   * @param acc
+   */
+  template <typename Particles>
+  void eval_newtonian_acc(Particles const &partc, typename Particles::Coord &acc);
 
-    /*---------------------------------------------------------------------------*\
-        Class Interactions Implementation
-    \*---------------------------------------------------------------------------*/
-    template<typename Derived>
-    template<typename Particles>
-    void Interactions<Derived>::eval_acc(const Particles &partc, typename Particles::Coord &acc) {
-        static_cast<Derived *>(this)->impl_eval_acc(partc, acc);
-    }
+ private:
+  // constructors
+  Interactions() = default;
 
-    template<typename Derived>
-    template<typename Particles>
-    void Interactions<Derived>::eval_extra_vel_dep_acc(const Particles &partc, typename Particles::Coord &acc) {
-        static_cast<Derived *>(this)->impl_eval_extra_vel_dep_acc(partc, acc);
-    }
+  friend Derived;
+};
 
-    template<typename Derived>
-    template<typename Particles>
-    void Interactions<Derived>::eval_extra_vel_indep_acc(const Particles &partc, typename Particles::Coord &acc) {
-        static_cast<Derived *>(this)->impl_eval_extra_vel_indep_acc(partc, acc);
-    }
-
-    template<typename Derived>
-    template<typename Particles>
-    void Interactions<Derived>::eval_newtonian_acc(const Particles &partc, typename Particles::Coord &acc) {
-        static_cast<Derived *>(this)->impl_eval_newtonian_acc(partc, acc);
-    }
-    /*---------------------------------------------------------------------------*\
-        Help functions and tools
-    \*---------------------------------------------------------------------------*/
-    template<typename T>
-    constexpr bool is_interactions_v = std::is_base_of_v<Interactions<T>, T>;
+/*---------------------------------------------------------------------------*\
+    Class Interactions Implementation
+\*---------------------------------------------------------------------------*/
+template <typename Derived>
+template <typename Particles>
+void Interactions<Derived>::eval_acc(const Particles &partc, typename Particles::Coord &acc) {
+  static_cast<Derived *>(this)->impl_eval_acc(partc, acc);
 }
 
-#endif //SPACEHUB_EOM_H
+template <typename Derived>
+template <typename Particles>
+void Interactions<Derived>::eval_extra_vel_dep_acc(const Particles &partc, typename Particles::Coord &acc) {
+  static_cast<Derived *>(this)->impl_eval_extra_vel_dep_acc(partc, acc);
+}
+
+template <typename Derived>
+template <typename Particles>
+void Interactions<Derived>::eval_extra_vel_indep_acc(const Particles &partc, typename Particles::Coord &acc) {
+  static_cast<Derived *>(this)->impl_eval_extra_vel_indep_acc(partc, acc);
+}
+
+template <typename Derived>
+template <typename Particles>
+void Interactions<Derived>::eval_newtonian_acc(const Particles &partc, typename Particles::Coord &acc) {
+  static_cast<Derived *>(this)->impl_eval_newtonian_acc(partc, acc);
+}
+/*---------------------------------------------------------------------------*\
+    Help functions and tools
+\*---------------------------------------------------------------------------*/
+template <typename T>
+constexpr bool is_interactions_v = std::is_base_of_v<Interactions<T>, T>;
+}  // namespace space
+
+#endif  // SPACEHUB_EOM_H
--- src/own-math.hpp
+++ src/own-math.hpp
@@ -2,157 +2,155 @@
 #ifndef OWN_MATH_H
 #define OWN_MATH_H
 
-#include "dev-tools.hpp"
 #include <math.h>
+#include "dev-tools.hpp"
 namespace space {
-    /** @brief Self min()*/
-    template<typename T1, typename T2>
-    inline const T2 min(const T1 &x, const T2 &y) {
-        return x > y ? y : x;
-    }
-
-    /** @brief Self max()*/
-    template<typename T1, typename T2>
-    inline const T2 max(const T1 &x, const T2 &y) {
-        return y > x ? y : x;
-    }
-
-    /** @brief Self abs()*/
-    template<typename T>
-    inline const T abs(const T &x) {
-        return x > 0 ? x : -x;
-    }
-
-    template <typename T>
-    inline T in_range(T low, T x, T high) {
-        T tmp = low > x ? low : x;
-        return tmp > high ? high : tmp;
-    }
-    /**
-     *
-     * @tparam T
-     * @param x
-     * @return
-     */
-    template<typename T>
-    inline constexpr T stepfunction(T x) {
-        return static_cast<T>(x > 0);
-    }
-
-    /**
-     *
-     * @tparam T
-     * @param x
-     * @return
-     */
-    template<typename T>
-    inline constexpr T sign(T x) {
-        return -1 + 2 * static_cast<T>(x > 0);
-    }
-
-    /**
-     *
-     * @tparam Dtype
-     */
-    template<typename Dtype>
-    struct epsilon {
-        using value_type = typename space::get_value_type<Dtype>::type;
-        constexpr static value_type value = std::numeric_limits<value_type>::epsilon();
-    };
-
-    template <typename T>
-    constexpr T epsilon_v = epsilon<T>::value;
-
-    template <typename T>
-    inline bool iseq(T x, T y){
-        return fabs(x-y) < epsilon_v<T>;
-    }
-
-    /**
-     *
-     * @tparam Dtype
-     */
-    template<typename Dtype>
-    struct max_value {
-        using value_type = typename space::get_value_type<Dtype>::type;
-        constexpr static value_type value = std::numeric_limits<value_type>::max();
-    };
-
-    /**
-     *
-     * @tparam Dtype
-     */
-    template<typename Dtype>
-    struct big_value {
-        using value_type = typename space::get_value_type<Dtype>::type;
-        constexpr static value_type value = 0.1 * std::numeric_limits<value_type>::max();
-    };
-
-    template<typename T>
-    inline T KarmackFastInverseSquareRoot(T x) {
-        return 1 / sqrt(x);
-    }
-
-    template<>
-    inline float KarmackFastInverseSquareRoot<float>(float x) {
-        float xhalf = 0.5f * x;
-        int i = *(int *) &x;
-        //i = 0x5f3759df - (i >> 1);
-        i = 0x5f375a86 - (i >> 1);
-        x = *(float *) &i;
-        x = x * (1.5f - xhalf * x * x);
-        //x = x*(1.5f - xhalf*x*x);
-        return x;
-    }
-
-    template<>
-    inline double KarmackFastInverseSquareRoot<double>(double x) {
-        double xhalf = 0.5f * x;
-        long long i = *(long long *) &x;
-        i = 0x5fe6eb50c7aa19f9 - (i >> 1);
-        x = *(double *) &i;
-        x = x * (1.5f - xhalf * x * x);
-        //x = x*(1.5f - xhalf*x*x);
-        return x;
-    }
-
-    /**
-     *
-     * @tparam Fun
-     * @param f
-     * @return
-     */
-    template<typename Fun>
-    decltype(std::declval<Fun>()(0)) root_dichotom(Fun f) {
-        using Scalar = decltype(f(0));
-        Scalar up = space::big_value<Scalar>::value;
-        Scalar low = -up;
-
-        for (; fabs((up - low)) > fabs(up) * space::epsilon<Scalar>::value;) {
-            Scalar mid = 0.5 * (up + low);
-            if (f(mid) > 0)
-                up = mid;
-            else
-                low = mid;
-        }
-        return 0.5 * (up + low);
-    }
-
-    template<typename Fun>
-    decltype(std::declval<Fun>()(0)) root_newton(Fun f) {
-        using Scalar = decltype(f(0));
-        constexpr size_t max_iter = 1000;
-        Scalar x0 = 0;
-        Scalar x = 1;
-        for(size_t i = 0 ; i < max_iter ; ++i){
-            x = x0 - f(x0);
-            if(fabs(x-x0) <= space::epsilon<Scalar>::value){
-                break;
-            } else{
-                x0 = x;
-            }
-        }
-        return x;
-    }
+/** @brief Self min()*/
+template <typename T1, typename T2>
+inline const T2 min(const T1 &x, const T2 &y) {
+  return x > y ? y : x;
 }
+
+/** @brief Self max()*/
+template <typename T1, typename T2>
+inline const T2 max(const T1 &x, const T2 &y) {
+  return y > x ? y : x;
+}
+
+/** @brief Self abs()*/
+template <typename T>
+inline const T abs(const T &x) {
+  return x > 0 ? x : -x;
+}
+
+template <typename T>
+inline T in_range(T low, T x, T high) {
+  T tmp = low > x ? low : x;
+  return tmp > high ? high : tmp;
+}
+/**
+ *
+ * @tparam T
+ * @param x
+ * @return
+ */
+template <typename T>
+inline constexpr T stepfunction(T x) {
+  return static_cast<T>(x > 0);
+}
+
+/**
+ *
+ * @tparam T
+ * @param x
+ * @return
+ */
+template <typename T>
+inline constexpr T sign(T x) {
+  return -1 + 2 * static_cast<T>(x > 0);
+}
+
+/**
+ *
+ * @tparam Dtype
+ */
+template <typename Dtype>
+struct epsilon {
+  using value_type = typename space::get_value_type<Dtype>::type;
+  constexpr static value_type value = std::numeric_limits<value_type>::epsilon();
+};
+
+template <typename T>
+constexpr T epsilon_v = epsilon<T>::value;
+
+template <typename T>
+inline bool iseq(T x, T y) {
+  return fabs(x - y) < epsilon_v<T>;
+}
+
+/**
+ *
+ * @tparam Dtype
+ */
+template <typename Dtype>
+struct max_value {
+  using value_type = typename space::get_value_type<Dtype>::type;
+  constexpr static value_type value = std::numeric_limits<value_type>::max();
+};
+
+/**
+ *
+ * @tparam Dtype
+ */
+template <typename Dtype>
+struct big_value {
+  using value_type = typename space::get_value_type<Dtype>::type;
+  constexpr static value_type value = 0.1 * std::numeric_limits<value_type>::max();
+};
+
+template <typename T>
+inline T KarmackFastInverseSquareRoot(T x) {
+  return 1 / sqrt(x);
+}
+
+template <>
+inline float KarmackFastInverseSquareRoot<float>(float x) {
+  float xhalf = 0.5f * x;
+  int i = *(int *)&x;
+  // i = 0x5f3759df - (i >> 1);
+  i = 0x5f375a86 - (i >> 1);
+  x = *(float *)&i;
+  x = x * (1.5f - xhalf * x * x);
+  // x = x*(1.5f - xhalf*x*x);
+  return x;
+}
+
+template <>
+inline double KarmackFastInverseSquareRoot<double>(double x) {
+  double xhalf = 0.5f * x;
+  long long i = *(long long *)&x;
+  i = 0x5fe6eb50c7aa19f9 - (i >> 1);
+  x = *(double *)&i;
+  x = x * (1.5f - xhalf * x * x);
+  // x = x*(1.5f - xhalf*x*x);
+  return x;
+}
+
+/**
+ *
+ * @tparam Fun
+ * @param f
+ * @return
+ */
+template <typename Fun>
+auto root_bisection(Fun f, decltype(f(0)) low, decltype(f(0)) high) -> decltype(f(0)) {
+  using Scalar = decltype(f(0));
+
+  for (; fabs((high - low)) > fabs(high) * space::epsilon<Scalar>::value;) {
+    Scalar mid = 0.5 * (high + low);
+    if (f(mid) > 0)
+      high = mid;
+    else
+      low = mid;
+  }
+  return 0.5 * (high + low);
+}
+
+template <typename Fun>
+decltype(std::declval<Fun>()(0)) root_newton(Fun f) {
+  using Scalar = decltype(f(0));
+  constexpr size_t max_iter = 1000;
+  Scalar x0 = 0;
+  Scalar x = 1;
+  for (size_t i = 0; i < max_iter; ++i) {
+    x = x0 - f(x0);
+    if (fabs(x - x0) <= space::epsilon<Scalar>::value) {
+      break;
+    } else {
+      x0 = x;
+    }
+  }
+  return x;
+}
+}  // namespace space
 #endif
