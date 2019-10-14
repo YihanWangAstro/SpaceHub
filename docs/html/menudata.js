f3f33de4133e153c8ca9202a2a4eb85591f425ff
YihanWangAstro <yihan.wang.1@stonybrook.edu>
2019-10-14 02:47:20 -0400

    add class graph

--- docs/html/menudata.js
+++ docs/html/menudata.js
@@ -30,7 +30,7 @@ var menudata={children:[
 {text:"Data Structures",url:"annotated.html",children:[
 {text:"Data Structures",url:"annotated.html"},
 {text:"Data Structure Index",url:"classes.html"},
-{text:"Class Hierarchy",url:"hierarchy.html"},
+{text:"Class Hierarchy",url:"inherits.html"},
 {text:"Data Fields",url:"functions.html",children:[
 {text:"All",url:"functions.html",children:[
 {text:"a",url:"functions.html#index_a"},
