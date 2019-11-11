872d88bc7627644c853a7b8bf837049a0fc3f356
YihanWangAstro <yihan.wang.1@stonybrook.edu>
2019-11-11 09:48:05 -0500

    update

---
+++ test/regression_test/rtest_samples.hpp
@@ -0,0 +1,22 @@
+
+#ifndef SPACEHUB_RTEST_SAMPLES_HPP
+#define SPACEHUB_RTEST_SAMPLES_HPP
+#include "../../src/orbits/orbits.hpp"
+#include "../../src/macros.hpp"
+
+template<typename Solver>
+auto two_body(double e) {
+  using Particle = typename Solver::Particle;
+  using namespace space;
+  using namespace space::unit;
+
+  Particle sun{1_Ms}, earth{1_Me};
+  auto orbit = orbit::EllipOrbit(sun.mass, earth.mass, 1_AU, e, 0, 0, 0, 0);
+
+  orbit::move_particles(orbit, earth);
+
+  orbit::move_to_COM_frame(sun, earth);
+
+  return std::make_tuple(sun, earth);
+}
+#endif //SPACEHUB_RTEST_SAMPLES_HPP
