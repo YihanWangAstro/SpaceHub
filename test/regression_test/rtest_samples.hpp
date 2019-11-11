2110283242f1278b893f1cfc6e95868d463d3042
YihanWangAstro <yihan.wang.1@stonybrook.edu>
2019-11-11 18:23:20 -0500

    update rtest sample

--- test/regression_test/rtest_samples.hpp
+++ test/regression_test/rtest_samples.hpp
@@ -2,6 +2,7 @@
 #ifndef SPACEHUB_RTEST_SAMPLES_HPP
 #define SPACEHUB_RTEST_SAMPLES_HPP
 #include "../../src/orbits/orbits.hpp"
+#include "../../src/orbits/particle-manip.hpp"
 #include "../../src/macros.hpp"
 
 template<typename Solver>
@@ -19,4 +20,38 @@ auto two_body(double e) {
 
   return std::make_tuple(sun, earth);
 }
+
+
+template<typename Solver>
+auto kozai(double e) {
+    using Particle = typename Solver::Particle;
+    using namespace space;
+    using namespace space::unit;
+
+    Particle sun{1_Ms}, earth{1_Me};
+    auto orbit = orbit::EllipOrbit(sun.mass, earth.mass, 1_AU, e, 0, 0, 0, 0);
+
+    orbit::move_particles(orbit, earth);
+
+    orbit::move_to_COM_frame(sun, earth);
+
+    return std::make_tuple(sun, earth);
+}
+
+template<typename Solver>
+auto outer_solar() {
+    using Particle = typename Solver::Particle;
+    using namespace space;
+    using namespace space::unit;
+
+    Particle sun{1_Ms}, earth{1_Me};
+    auto orbit = orbit::EllipOrbit(sun.mass, earth.mass, 1_AU, e, 0, 0, 0, 0);
+
+    orbit::move_particles(orbit, earth);
+
+    orbit::move_to_COM_frame(sun, earth);
+
+    return std::make_tuple(sun, earth);
+}
+
 #endif //SPACEHUB_RTEST_SAMPLES_HPP
