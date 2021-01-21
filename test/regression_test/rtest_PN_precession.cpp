1ef951236537fab8595e600df2abee4e7a0a41ff
Yihan <yihan.wang.1@stonybrook.edu>
2021-01-21 13:45:23 -0500

    update test file

--- test/regression_test/rtest_PN_precession.cpp
+++ test/regression_test/rtest_PN_precession.cpp
@@ -77,9 +77,7 @@ void run(std::string const &sim_type) {
 int main(int argc, char **argv) {
     using namespace interactions;
 
-    using type = Types<double>;
-
-    using force = Interactions<NewtonianGrav, PN1, PN2>;
+    using force = interactions::Interactions<interactions::NewtonianGrav>;
 
     using particles = PointParticles<type>;
 
@@ -91,9 +89,16 @@ int main(int argc, char **argv) {
 
     using arch_sys = ARchainSystem<particles, force, ReguType::LogH>;
 
-    // using iter = ConstOdeIterator<Symplectic2nd>;
+    using base_integrator = LeapFrogDKD<type>;
+    //    using iter = ConstOdeIterator<Symplectic2nd>;
 
-    using iter = BurlishStoer<double, WorstOffender, PIDController>;
+    using err_estimator = WorstOffender<type>;
+
+    using step_controller = PIDController<type>;
+
+    using iter = BurlishStoer<base_integrator, err_estimator, step_controller>;
+
+    using ias15_iter = IAS15<type, IAS15Error<type>, step_controller>;
 
     run<Simulator<sim_sys, iter>>("sim");
 
