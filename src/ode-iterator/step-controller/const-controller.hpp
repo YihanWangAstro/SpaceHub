75b80cbfea05ac14231cfabbe384a078824d35f8
Yihan <yihan.wang.1@stonybrook.edu>
2021-01-22 23:36:21 -0500

    add const controller

---
+++ src/ode-iterator/step-controller/const-controller.hpp
@@ -0,0 +1,49 @@
+/*---------------------------------------------------------------------------*\
+        .-''''-.         |
+       /        \        |
+      /_        _\       |  SpaceHub: The Open Source N-body Toolkit
+     // \  <>  / \\      |
+     |\__\    /__/|      |  Website:  https://yihanwangastro.github.io/SpaceHub/
+      \    ||    /       |
+        \  __  /         |  Copyright (C) 2019 Yihan Wang
+         '.__.'          |
+---------------------------------------------------------------------
+License
+    This file is part of SpaceHub.
+    SpaceHub is free software: you can redistribute it and/or modify it under
+    the terms of the MIT License. SpaceHub is distributed in the hope that it
+    will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+    of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the MIT License
+    for more details. You should have received a copy of the MIT License along
+    with SpaceHub.
+\*---------------------------------------------------------------------------*/
+/**
+ * @file const-controller.hpp
+ *
+ * Header file.
+ */
+#pragma once
+
+#include "../../math.hpp"
+
+namespace space::ode_iterator {
+    /*---------------------------------------------------------------------------*\
+         Class Const step Controller Declaration
+    \*---------------------------------------------------------------------------*/
+    /**
+     * @tparam T
+     */
+    template <typename TypeSystem>
+    class ConstStepController {
+       public:
+        // Type member
+        SPACEHUB_USING_TYPE_SYSTEM_OF(TypeSystem);
+
+        template <typename ArrayLike>
+        inline Scalar next_step_size(size_t order, Scalar old_step, ArrayLike const &errors) {
+            return old_step;
+        };
+
+        inline Scalar next_step_size(size_t order, Scalar old_step, Scalar error) { return old_step; };
+    };
+}  // namespace space::ode_iterator
