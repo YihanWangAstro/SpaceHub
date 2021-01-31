a5e7bd39c4f2ca9774b320b008c1dcfab96a11fd
Yihan <yihan.wang.1@stonybrook.edu>
2021-01-30 19:37:30 -0500

    code inspector test

--- test/regression_test/rtest_samples.hpp
+++ test/regression_test/rtest_samples.hpp
@@ -48,17 +48,17 @@ auto outer_solar() {
     using namespace space::unit;
     using namespace space::orbit;
 
-    Particle sun{1.00000597682_Ms, -4.06428567034226e-3, -6.08813756435987e-3, -1.66162304225834e-6,
+    Particle sun{1.00000597682_Ms,     -4.06428567034226e-3, -6.08813756435987e-3, -1.66162304225834e-6,
                  +6.69048890636161e-6, -6.33922479583593e-6, -3.13202145590767e-9};
-    Particle jup{1. / 1047.355, +3.40546614227466e+0, +3.62978190075864e+0, +3.42386261766577e-2,
-                 -0.3254242234844626, 0.32078376079804843, -0.00015504584274327233};
-    Particle sat{1. / 3501.6, +6.60801554403466e+0, +6.38084674585064e+0, -1.36145963724542e-1,
-                 -0.24261807906125546, 0.23236917361652312, 0.0009720111543216125};
-    Particle ura{1. / 22869., +1.11636331405597e+1, +1.60373479057256e+1, +3.61783279369958e-1,
-                 -0.1894447922304644, 0.12000768830940597, -0.0012655376696374542};
-    Particle nep{1. / 19314., -3.01777243405203e+1, +1.91155314998064e+0, -1.53887595621042e-1,
+    Particle jup{1. / 1047.355,       +3.40546614227466e+0, +3.62978190075864e+0,   +3.42386261766577e-2,
+                 -0.3254242234844626, 0.32078376079804843,  -0.00015504584274327233};
+    Particle sat{1. / 3501.6,          +6.60801554403466e+0, +6.38084674585064e+0, -1.36145963724542e-1,
+                 -0.24261807906125546, 0.23236917361652312,  0.0009720111543216125};
+    Particle ura{1. / 22869.,         +1.11636331405597e+1, +1.60373479057256e+1,  +3.61783279369958e-1,
+                 -0.1894447922304644, 0.12000768830940597,  -0.0012655376696374542};
+    Particle nep{1. / 19314.,          -3.01777243405203e+1, +1.91155314998064e+0, -1.53887595621042e-1,
                  -0.01264216568441132, -0.18100181375010865, 0.0020831452402001265};
-    Particle plu{7.4074074e-09, -2.13858977531573e+1, +3.20719104739886e+1, +2.49245689556096e+0,
+    Particle plu{7.4074074e-09,        -2.13858977531573e+1, +3.20719104739886e+1, +2.49245689556096e+0,
                  -0.10285755114348066, -0.12017192726456442, 0.038256490292647924};
 
     move_to_COM_frame(sun, jup, sat, ura, nep);
@@ -113,8 +113,8 @@ auto kozai() {
 }
 
 template <typename Solver>
-double basic_error_test(std::string const &fname, double end_time, double rtol,
-                        std::vector<typename Solver::Particle> const p) {
+double basic_error_test(std::string const& fname, double end_time, double rtol,
+                        std::vector<typename Solver::Particle> const& p) {
     using namespace space;
     using namespace run_operations;
     using namespace tools;
@@ -141,13 +141,13 @@ double basic_error_test(std::string const &fname, double end_time, double rtol,
 
     // args.atol = args.rtol;
     args.add_operation(TimeSlice(
-            [&](auto &ptc, auto step_size) {
-                auto err = calc::calc_energy_error(ptc, E0);
-                tot_error += err * err;
-                error_num++;
-                err_file << ptc.time() << ',' << err << '\n';
-            },
-            0, end_time));
+        [&](auto& ptc, auto step_size) {
+            auto err = calc::calc_energy_error(ptc, E0);
+            tot_error += err * err;
+            error_num++;
+            err_file << ptc.time() << ',' << err << '\n';
+        },
+        0, end_time));
 
     // args.add_operation(TimeSlice(DefaultWriter(fname + ".txt"), 0, end_time, 10000));
 
@@ -163,7 +163,7 @@ double basic_error_test(std::string const &fname, double end_time, double rtol,
 }
 
 template <typename Solver>
-double bench_mark(double end_time, double rtol, std::vector<typename Solver::Particle> const p) {
+double bench_mark(double end_time, double rtol, std::vector<typename Solver::Particle> const& p) {
     using namespace space;
     using namespace run_operations;
     using namespace tools;
@@ -193,7 +193,7 @@ double bench_mark(double end_time, double rtol, std::vector<typename Solver::Par
 }
 
 template <typename Solver>
-auto error_scale(double rtol_start, double rtol_end, double end_time, std::vector<typename Solver::Particle> const p) {
+auto error_scale(double rtol_start, double rtol_end, double end_time, std::vector<typename Solver::Particle> const& p) {
     using namespace space;
     using namespace run_operations;
     using namespace tools;
@@ -212,7 +212,7 @@ auto error_scale(double rtol_start, double rtol_end, double end_time, std::vecto
 
         auto E0 = orbit::E_tot(p);  // calc::calc_total_energy(sim.particles());
 
-        args.add_pre_step_operation([&](auto &ptc, auto step_size) {
+        args.add_pre_step_operation([&](auto& ptc, auto step_size) {
             auto err = calc::calc_energy_error(ptc, E0);
             tot_error += err * err;
             // std::cout << ' ' << err << ' ' << error_num << ' ' << thid << '\n';
