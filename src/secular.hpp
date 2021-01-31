8da19c156a38d06842788ac078e7ab04b89e65eb
Yihan <yihan.wang.1@stonybrook.edu>
2021-01-30 19:11:43 -0500

    secular init

---
+++ src/secular.hpp
@@ -0,0 +1,39 @@
+/*---------------------------------------------------------------------------*\
+        .-''''-.         |
+       /        \        |
+      /_        _\       |  SpaceHub: The Open Source N-body Toolkit
+     // \  <>  / \\      |
+     |\__\    /__/|      |  Website:  https://yihanwangastro.github.io/SpaceHub/
+      \    ||    /       |
+        \  __  /         |  Copyright (C) 2019 Yihan Wang
+         '.__.'          |
+---------------------------------------------------------------------
+License
+    This file is part of SpaceHub.
+    SpaceHub is free software: you can redistribute it and/or modify it under
+    the terms of the MIT License. SpaceHub is distributed in the hope that it
+    will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+    of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the MIT License
+    for more details. You should have received a copy of the MIT License along
+    with SpaceHub.
+\*---------------------------------------------------------------------------*/
+/**
+ * @file secular.hpp
+ *
+ * Header file.
+ */
+#pragma once
+#include "macros.hpp"
+
+namespace space::secular {
+
+    template <typename T = double>
+    T GW_dadt(){
+      
+    }
+
+    template <typename T = double>
+    T radial_tidal_dadt(T m, T M, T R, T a, T e, T k, T tau) {
+
+    }
+}  // namespace space::secular
