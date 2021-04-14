402578a9a820c5cf7b10cc6042feb2a735e14d18
Yihan <yihan.wang.1@stonybrook.edu>
2021-04-14 15:04:04 -0400

    add earth radius unit

--- src/macros.hpp
+++ src/macros.hpp
@@ -61,6 +61,7 @@ namespace hub::unit {
     MAKE_UNIT(km, 1_AU / 149597870.7)
     MAKE_UNIT(PC, 1_AU * 648000.0 / consts::pi)
     MAKE_UNIT(Rs, 6.957e5_km)
+    MAKE_UNIT(Re, 6378_km)
     MAKE_UNIT(Rj, 69911_km)
 
     MAKE_UNIT(kms, km / sec)
